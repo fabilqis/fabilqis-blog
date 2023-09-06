b643e97489855ee50a5c0cc32afe487d39336ae3
unknown <fadillah.bilqis@gmail.com>
2023-09-06 14:35:13 +0700

    Added my project

---
+++ data/projectsData.ts
@@ -0,0 +1,28 @@
+const projectsData = [
+  {
+    title: 'Landing Page : Re:Work AI',
+    description: `My previous company ask me to build their landing page, I used simple stack to build it. Responsive and nice palette`,
+    imgSrc: '/static/images/rework.png',
+    href: 'https://porto-one.fabilqis.my.id',
+  },
+  {
+    title: 'Landing Page : Qomm.io',
+    description: `Same story, this is my previous company, also want me to create their landing page. Responsive and the design from Qomm itself`,
+    imgSrc: '/static/images/qomm.png',
+    href: 'https://porto-two.fabilqis.my.id',
+  },
+  {
+    title: 'Blog Page : CoSe Clinic',
+    description: `To deliver chatbot page, I have task to create a blog page of CoSe Clinic, Responsive and clean design, following the master-site`,
+    imgSrc: '/static/images/cose.png',
+    href: 'https://porto-three.fabilqis.my.id',
+  },
+  {
+    title: 'Landing Page : English Cafe',
+    description: `Helping my brother's friend to established her business through minimalism website looks, also responsive and connected to mailto`,
+    imgSrc: '/static/images/engcafe.png',
+    href: 'https://porto-four.fabilqis.my.id',
+  },
+]
+
+export default projectsData
