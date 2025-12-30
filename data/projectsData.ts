6be3f8c0a3978952b8ec7a006868a34a1dca67d1
Fadillah Bilqis <fadillah.bilqis@gmail.com>
2025-12-30 12:21:53 +0700

    Update project links and titles in projectsData

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,25 +3,25 @@ const projectsData = [
     title: 'Landing Page : Re:Work AI',
     description: `My previous company ask me to build their landing page, I used simple stack to build it. Responsive and nice palette`,
     imgSrc: '/static/images/rework.png',
-    href: 'https://porto-one.fabilqis.my.id',
+    href: 'https://reai-two.vercel.app',
   },
   {
     title: 'Landing Page : Qomm.io',
     description: `Same story, this is my previous company, also want me to create their landing page. Responsive and the design from Qomm itself`,
     imgSrc: '/static/images/qomm.png',
-    href: 'https://porto-two.fabilqis.my.id',
+    href: 'https://qomm-landing.vercel.app/',
   },
   {
     title: 'Blog Page : CoSe Clinic',
     description: `To deliver chatbot page, I have task to create a blog page of CoSe Clinic, Responsive and clean design, following the master-site`,
     imgSrc: '/static/images/cose.png',
-    href: 'https://porto-three.fabilqis.my.id',
+    href: 'https://cose-kappa.vercel.app/',
   },
   {
-    title: 'Landing Page : English Cafe',
+    title: 'Landing Page : Skyfastnusantara',
     description: `Helping my brother's friend to established her business through minimalism website looks, also responsive and connected to mailto`,
     imgSrc: '/static/images/engcafe.png',
-    href: 'https://porto-four.fabilqis.my.id',
+    href: 'https://skyfastnusantara.com/',
   },
 ]
 
